0a77c7d65ed6c35c35c5186bd31c4dbe5d65eafb
Utkarsh0402 <chaturvedyutkarsh800@gmail.com>
2026-04-13 10:44:10 +0530

    issue fix

--- jwels/settings.py
+++ jwels/settings.py
@@ -1,3 +1,8 @@
+STATIC_URL = 'static/'
+
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 """
 Django settings for jwels project.
 
@@ -48,6 +53,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # 👈 ye add karo
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    ...
 ]
 
 ROOT_URLCONF = 'jwels.urls'
